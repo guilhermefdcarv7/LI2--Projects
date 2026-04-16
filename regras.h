2b081cc2d8c297da11413f5b897a50658bcfec3b
Guilherme Carvalho <guicarvalho2611@gmail.com>
2026-04-16 16:14:41 +0100

    Delete regras.h

--- regras.h
+++
@@ -1,7 +0,0 @@
-#include "estrutura.h"
-
-int pode_jogar(struct Carta carta_mesa, struct Carta carta_descarte);
-
-int existe_jogada_possivel(struct Mesa mesa, int jogada_possivel);
-
-int verifica_estado_jogo(struct Mesa mesa);
