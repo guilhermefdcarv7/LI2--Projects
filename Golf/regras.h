4d4101246e6020a3085517d7be1bb1f7f5a09392
Guilherme Carvalho <guicarvalho2611@gmail.com>
2026-04-16 16:13:19 +0100

    Add files via upload

---
+++ Golf/regras.h
@@ -0,0 +1,7 @@
+#include "estrutura.h"
+
+int pode_jogar(struct Carta carta_mesa, struct Carta carta_descarte);
+
+int existe_jogada_possivel(struct Mesa mesa, int jogada_possivel);
+
+int verifica_estado_jogo(struct Mesa mesa);
